f92f65c11724cc0f0ad070f191b23559530ee81f
Elliot Kim <elliotkim916@gmail.com>
2019-04-29 15:58:34 -0700

    Added event listener for window

--- index.js
+++ index.js
@@ -2,5 +2,13 @@
 
 // listen on window for keydown event, and when it happens function will run
 window.addEventListener('keydown', e => {
-  console.log(e);
+  // we want to find an audio element on the page that has a data-key attribute of 65, 42, etc..
+  // using attribute selector 
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  console.log(audio);
+  if (!audio) {
+    // stops the function from running altogether if the key user presses has no data-key attribute
+    return;
+  }
+  audio.play();
 });
