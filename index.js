011205eada2275faed589aca880f8b33a4c09dca
Elliot Kim <elliotkim916@gmail.com>
2019-04-29 17:00:20 -0700

    Added playSound and removeTransition functions

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
 'use strict';
 
-// listen on window for keydown event, and when it happens function will run
-window.addEventListener('keydown', e => {
+function playSound(e) {
   // we want to find an audio element on the page that has a data-key attribute of 65, 42, etc..
   // using attribute selector 
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-  console.log(audio);
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+
   if (!audio) {
-    // stops the function from running altogether if the key user presses has no data-key attribute
-    return;
+    return; // stops the function from running altogether if the key user presses has no data-key attribute
+  } else {
+    audio.currentTime = 0; // rewinds to the start
+    audio.play();
+    key.classList.add('playing');
   }
-  audio.play();
-});
+}
+
+function removeTransition(e) {
+  if (e.propertyName !== 'transform') {
+    return; 
+  } else {
+    this.classList.remove('playing');
+  }
+}
+
+// When you have an array of elements, you cannot listen on all of them
+// You must explicitly loop over every single one of them and attach an event listener
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+// listen on window for keydown event, and when it happens function will run
+window.addEventListener('keydown', playSound);
